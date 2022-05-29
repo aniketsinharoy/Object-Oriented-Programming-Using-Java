bb79f166b7f5c9c97153f39d5b47fccc1238cf6d
Aniket Sinha Roy <73071212+aniketsinharoy@users.noreply.github.com>
2022-05-29 20:51:00 +0530

    Add files via upload
    The client initiates communication with a server that is passively waiting to be contacted. The typical TCP client goes through three steps:
    1. Construct an instance of Socket: The constructor establishes a TCP connection to the
    specified remote host and port.
    2. C o m m u n i c a t e using the socket's I/O streams: A c o n n e c t e d instance of Socket contains
    an InputStream a n d 0utputStream that can be u s e d j u s t like any other Java I/O s t r e a m.
    3. Close the c o n n e c t i o n using the c l o s e ( ) m e t h o d of Socket.

---
+++ Computer Networks/Echo Server/Client.java
@@ -0,0 +1,45 @@
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
+public class Client
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Socket clientSocket = new Socket("localhost",1077);         //port no same as server port
+
+            Scanner sc = new Scanner(System.in);
+            String sentence; 
+            System.out.print("Enter the sentence to be echo: ");
+            sentence = sc.nextLine();
+
+            byte[] byteSentence = sentence.getBytes(); //converting string into byte form since while transferring data bytes are used
+
+            InputStream in = clientSocket.getInputStream();
+            OutputStream out = clientSocket.getOutputStream();
+
+            out.write(byteSentence);                //transfering data to server
+
+            int totalByteReceived=0;                //total bytes received from server
+            int currentNoOfByteReceived;            //current no of bytes received from server at a particular time
+
+            while(totalByteReceived<byteSentence.length)
+            {
+                if((currentNoOfByteReceived = in.read(byteSentence,totalByteReceived,byteSentence.length - totalByteReceived))==-1)
+                {
+                    throw new SocketException("Connection closed prematurely");             //1st arg -> place to hold data from server
+                }                                                                           //2nd arg -> no of bytes got till now
+                totalByteReceived+=currentNoOfByteReceived;                                 //3rd arg -> remeaning no of bytes left
+            }
+
+            System.out.println("Echoed Sentence: "+ new String(byteSentence));          //converting byte message into string
+
+            clientSocket.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error!!!"+" "+e.getMessage());
+        } 
+    }
+}

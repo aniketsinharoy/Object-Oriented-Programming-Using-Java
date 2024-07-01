7b10469a93be37e38f5406beffdc57e735d2f98b
Aniket Sinha Roy <73071212+aniketsinharoy@users.noreply.github.com>
2024-07-02 00:37:50 +0530

    Create LeftSiftAndRightShiftLinkList.java
    Left shifting and right shifting a linked list

---
+++ Linked List/Singly Linked List/LeftSiftAndRightShiftLinkList.java
@@ -0,0 +1,164 @@
+class Node {
+
+    private String data;
+    private Node next;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+      
+    public void setNext(Node node) {
+        this.next = node;
+    }
+      
+    public String getData() {
+        return this.data;
+    }
+      
+    public Node getNext() {
+        return this.next;
+    }
+}
+
+
+class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public Node getHead() {
+        return this.head;
+    }
+      
+    public void setTail(Node n) {
+        this.tail=n;
+    }
+
+	public void setHead(Node n) {
+        this.head=n;
+    }
+      
+    public Node getTail() {
+        return this.tail;
+    }
+
+    public void addAtEnd(String data) {
+        Node node = new Node(data);
+
+        if (this.head == null) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.setNext(node);
+
+            this.tail = node;
+        }
+    }
+
+    public void addAtBeginning(String data) {
+        Node node = new Node(data);
+
+        if (this.head == null) {
+            this.head = this.tail = node;
+        }
+
+        else {
+            node.setNext(this.head);
+            this.head = node;
+        }
+    }
+
+      public void display() {
+        Node temp = this.head;
+
+        while (temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+    }
+
+    public void insert(String data, String dataBefore) {
+        Node node = new Node(data);
+
+        if (this.head == null)
+            this.head = this.tail = node;
+        else {
+            Node nodeBefore = this.find(dataBefore);
+            if (nodeBefore != null) {
+                node.setNext(nodeBefore.getNext());
+                nodeBefore.setNext(node);
+                if (nodeBefore == this.tail)
+                    this.tail = node;
+            } 
+            else
+                System.out.println("Node not found");
+        }
+    }
+
+}
+
+
+class Tester {
+
+    public static void main(String args[]) {
+        LinkedList linkedList1 = new LinkedList();
+        linkedList1.addAtEnd("ABC");
+        linkedList1.addAtEnd("DFG");
+        linkedList1.addAtEnd("XYZ");
+        linkedList1.addAtEnd("EFG");
+            
+        LinkedList linkedList2 = new LinkedList();
+        linkedList2.addAtEnd("ABC");
+        linkedList2.addAtEnd("DFG");
+        linkedList2.addAtEnd("XYZ");
+        linkedList2.addAtEnd("EFG");
+            
+        System.out.println("Initial List");
+        linkedList1.display();
+
+        System.out.println("\nList after left shifting by 2 positions");
+        shiftListLeft(linkedList1, 2);
+        linkedList1.display();
+            
+        System.out.println("\nInitial List");
+        linkedList2.display();
+            
+        System.out.println("\nList after right shifting by 2 positions");
+        shiftListRight(linkedList2, 2);
+        linkedList2.display();
+    }
+
+    public static void shiftListLeft(LinkedList linkedList, int n) {
+                                                                            //left shifting
+		Node tempHead = linkedList.getHead();
+		int count = 0;
+		while(tempHead!=null){
+			++count;
+			tempHead = tempHead.getNext();
+		}
+		Tester.shiftListRight(linkedList, (count - n));
+    }
+
+    public static void shiftListRight(LinkedList linkedList, int n) {
+                                                                        //right shifting
+		Node tempHead = linkedList.getHead();
+		Node tempTail = linkedList.getTail();
+		Node previous = null;
+		int count = 0;
+		while(tempHead!=null){
+			++count;
+			previous = tempHead;
+			tempHead=tempHead.getNext();
+			if(count==n){
+				break;
+			}
+		}
+		tempTail.setNext(linkedList.getHead());
+		previous.setNext(null);
+		linkedList.setHead(tempHead);
+		linkedList.setTail(previous);
+    }
+}
